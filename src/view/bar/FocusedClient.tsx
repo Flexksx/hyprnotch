ec22050283ea60b5eaef509c304d1c0e7ed68678
Flexksx <ccretu@extole.com>
2025-06-07 12:45:22 +0300

    add workspace buttons

--- src/view/bar/FocusedClient.tsx
+++ src/view/bar/FocusedClient.tsx
@@ -3,15 +3,20 @@ import FocusedClientViewModel from "../../hyprland/FocusedClientViewModel";
 export default function FocusedClient() {
   const focusedClientViewModel = new FocusedClientViewModel();
   return (
-    <button
-      className="focused_client"
-      child={focusedClientViewModel.getFocusedClient().as((client) => {
-        if (client) {
-          return <label>{client.get_initial_title()}</label>;
-        } else {
-          return <label>No focused client</label>;
-        }
-      })}
-    ></button>
+    <box
+      className={"focused_client_container"}
+      child={
+        <button
+          className="focused_client"
+          child={focusedClientViewModel.getFocusedClient().as((client) => {
+            if (client) {
+              return <label label={client.get_initial_title()} />;
+            } else {
+              return <label label={"No focused client"} />;
+            }
+          })}
+        />
+      }
+    />
   );
 }
--- src/view/bar/WorkspacesBar.tsx
+++ src/view/bar/WorkspacesBar.tsx
@@ -17,42 +17,51 @@ export default function WorkspacesBar(props: WorkspacesBarProps) {
   const workspacesBinding = gdkmonitor.get_model()
     ? workspaceViewModel.getPerMonitorWorkspaces(gdkmonitor.get_model() || "")
     : workspaceViewModel.getWorkspaces();
+
   return (
     <box
-      className={"workspaces_bar"}
-      children={workspacesBinding.as((workspaces: Hyprland.Workspace[]) => {
-        logger.debug(
-          "WorkspacesBar updated with new workspaces. Current workspaces: ",
-          workspaces
-            .map((workspace: Hyprland.Workspace) => {
-              return workspace.get_name();
-            })
-            .toString()
-        );
-        return workspaces.map((workspace: Hyprland.Workspace) => {
-          return (
-            <button
-              className={workspaceViewModel
-                .getFocusedWorkspace()
-                .as((focusedWorkspace: Hyprland.Workspace) => {
-                  return (
-                    "workspace_button " +
-                    (focusedWorkspace.get_id() === workspace.get_id()
-                      ? "focused"
-                      : "")
-                  );
-                })}
-              child={<label>{workspace.get_name()}</label>}
-              onClick={() => {
-                logger.debug(
-                  `Pressing button for workspace ${workspace.get_id()}, switching to it`
-                );
-                workspaceViewModel.switchToWorkspace(workspace.get_id());
-              }}
-            ></button>
-          );
-        });
-      })}
-    ></box>
+      className="workspaces_bar_container"
+      child={
+        <box
+          className={"workspaces_bar"}
+          children={workspacesBinding.as((workspaces: Hyprland.Workspace[]) => {
+            logger.debug(
+              "WorkspacesBar updated with new workspaces. Current workspaces: ",
+              workspaces
+                .sort((a: Hyprland.Workspace, b: Hyprland.Workspace) => {
+                  return a.get_id() - b.get_id();
+                })
+                .map((workspace: Hyprland.Workspace) => {
+                  return workspace.get_name();
+                })
+                .toString()
+            );
+            return workspaces.map((workspace: Hyprland.Workspace) => {
+              return (
+                <button
+                  className={workspaceViewModel
+                    .getFocusedWorkspace()
+                    .as((focusedWorkspace: Hyprland.Workspace) => {
+                      return (
+                        "workspace_button " +
+                        (focusedWorkspace.get_id() === workspace.get_id()
+                          ? "focused"
+                          : "")
+                      );
+                    })}
+                  child={<label label={workspace.get_name()} />}
+                  onClick={() => {
+                    logger.debug(
+                      `Pressing button for workspace ${workspace.get_id()}, switching to it`
+                    );
+                    workspaceViewModel.switchToWorkspace(workspace.get_id());
+                  }}
+                ></button>
+              );
+            });
+          })}
+        />
+      }
+    />
   );
 }
