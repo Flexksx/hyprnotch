349edf4b2aac780c8e24696f227376400a2d9bff
Flexksx <ccretu@extole.com>
2025-06-23 22:49:41 +0300

    upd sound settings

--- src/view/notch/sound_settings/SoundSettingsNotch.tsx
+++ src/view/notch/sound_settings/SoundSettingsNotch.tsx
@@ -1,5 +1,22 @@
 import NotchStateViewModel from "../../../notch/state/NotchStateViewModel";
 
 export default function SoundSettingsNotch() {
-  return <button label={"Sound settings"} />;
+  return (
+    <box
+      className="sound_settings_notch"
+      child={
+        <box
+          className="sound_settings_notch_content"
+          vertical={true}
+          children={[
+            <label label="Sound Settings" className="sound_settings_label" />,
+            <label
+              label="Adjust your sound settings here."
+              className="sound_settings_description"
+            />,
+          ]}
+        />
+      }
+    /p>
+  );
 }
